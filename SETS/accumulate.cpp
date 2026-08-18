a97ade9bab2921143607007352d614727c94da7b
PRISHA <saxena.prisha2901@gmail.com>
2026-08-18 10:23:04 +0530

    accumulate

---
+++ SETS/accumulate.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<set>
+#include<numeric>
+using namespace std;
+
+int main(){
+
+    set<int> s;
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        s.insert(x);
+    }
+
+    int sum=accumulate(s.begin(),s.end(),0);
+
+    cout<<sum;
+}
