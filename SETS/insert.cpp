c8f67e2d14b4e567a423bc7f7f0b030d51e6f727
PRISHA <saxena.prisha2901@gmail.com>
2026-08-18 10:25:45 +0530

    insert

---
+++ SETS/insert.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main(){
+
+    set<int> s;
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        s.insert(x);
+    }
+
+    for(auto x:s)
+        cout<<x<<" ";
+}
