d55472f7ef8f8c0d6da33c8002c13aa7a1104a94
PRISHA <saxena.prisha2901@gmail.com>
2026-08-18 10:28:19 +0530

    union

---
+++ SETS/OPS/union.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<set>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    set<int> s1,s2,result;
+
+    int n,m;
+
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        s1.insert(x);
+    }
+
+    cin>>m;
+    for(int i=0;i<m;i++){
+        int x;
+        cin>>x;
+        s2.insert(x);
+    }
+
+    set_union(s1.begin(),s1.end(),
+              s2.begin(),s2.end(),
+              inserter(result,result.begin()));
+
+    for(auto x:result)
+        cout<<x<<" ";
+}
