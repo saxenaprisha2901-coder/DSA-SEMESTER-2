4ee152e2c4ac0c0a76ef611f51c343f42e29df3c
PRISHA <saxena.prisha2901@gmail.com>
2026-08-18 10:24:51 +0530

    find

---
+++ SETS/find.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main(){
+
+    set<int> s;
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        s.insert(x);
+    }
+
+    int key;
+    cin>>key;
+
+    if(s.find(key)!=s.end())
+        cout<<"Found";
+    else
+        cout<<"Not Found";
+}
