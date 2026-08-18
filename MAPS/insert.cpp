c19b06a6ad7f100d7fbf5dc442a7df6f47af21ee
PRISHA <saxena.prisha2901@gmail.com>
2026-08-18 10:32:57 +0530

    insert

---
+++ MAPS/insert.cpp
@@ -0,0 +1,57 @@
+//ordered
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+
+    map<string,int> CountryDatabase;
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+
+        string key;
+        int value;
+
+        cin>>key>>value;
+
+        CountryDatabase[key] = value;
+    }
+
+    for(auto x:CountryDatabase){
+        cout<<x.first<<" "<<x.second<<endl;
+    }
+
+    return 0;
+}
+
+//unordered
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+
+    unordered_map<string,int> CountryDatabase;
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+
+        string key;
+        int value;
+
+        cin>>key>>value;
+
+        CountryDatabase[key] = value;
+    }
+
+    for(auto x:CountryDatabase){
+        cout<<x.first<<" "<<x.second<<endl;
+    }
+
+    return 0;
+}
